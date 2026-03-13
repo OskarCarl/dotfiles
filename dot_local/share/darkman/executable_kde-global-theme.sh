c2371e4a06a305633e289f6a09d1305828cd520a
Oskar Carl <oskar.carl@uni-due.de>
2026-03-13 11:20:03 +0100

    Switch auto dark mode to darkman

---
+++ dot_local/share/darkman/executable_kde-global-theme.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Change the global Plasma Theme. On Manjaro you can use "org.manjaro.breath-dark.desktop"
+# or "org.manjaro.breath-light.desktop", or you can create your own global Plasma Theme
+# with the "Plasma Look And Feel Explorer".
+# Reference: https://userbase.kde.org/Plasma/Create_a_Global_Theme_Package
+
+case "$1" in
+dark) THEME=org.kde.breezedark.desktop ;;
+light) THEME=org.kde.breeze.desktop ;;
+default) exit 1 ;;
+esac
+
+plasma-apply-lookandfeel -a "$THEME"
+
