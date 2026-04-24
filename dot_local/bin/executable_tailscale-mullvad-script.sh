f9076ea0de0e0116f6142a28b904c432286665c4
Oskar Carl <oskar.carl@uni-due.de>
2026-04-24 15:09:58 +0200

    Add tailscale mullvad script

---
+++ dot_local/bin/executable_tailscale-mullvad-script.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+# Adapted from https://github.com/tailscale-support/mullvad-script
+
+# Store the command in a variable
+tsBinary=$(which tailscale)
+
+# Prompt the user to confirm that they want to sign multiple nodes
+printf "Do you want to sign multiple Mullvad nodes?\n"
+printf "Y/y to confirm: "
+read -r choice0
+printf "\n" # To move to a new line after input
+
+if [ "$choice0" = "Y" ] || [ "$choice0" = "y" ]; then
+  # Ask which country nodes to sign
+  printf '\nInput the two letter country code for the Mullvad exit nodes you would like to sign.\nUse ".." to sign all nodes.\n'
+  read -r country
+  if [ ${#country} -gt 2 ]; then
+    printf "Country code longer than 2 characters, please only enter 2 characters.\n"
+    exit 1
+  fi
+  # Store all unsigned nodes
+  lockedNodes=$($tsBinary lock status --json | jq -rc '.FilteredPeers[]')
+  # Loop through all locked nodes
+  echo "$lockedNodes" | while read -r node; do
+    nodeName=$(echo "$node" | jq -r .DNSName)
+    if echo "$nodeName" | grep -q "^$country.*mullvad\.ts\.net\.$"; then
+      printf "Signing node %s\n" "$nodeName"
+      "$tsBinary" lock sign "$(echo "$node" | jq -r '.NodeKey')"
+    else
+      # If verbose logging is enabled, show skipped nodes
+      if [ "$1" = "-v" ]; then
+        printf "Node %s skipped\n" "$nodeName"
+      fi
+    fi
+  done
+else
+  printf "\n'%s' not 'Y' or 'y'. Exiting...\n" "$choice0"
+  exit 1
+fi
